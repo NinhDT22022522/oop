3abe90ac9e567e46374d2ec9712685939502eeef
Dam Thai Ninh <22022522@vnu.edu.vn>
2023-03-12 16:32:04 +0700

    second commit

---
+++ HW1/HW1_B.java
@@ -0,0 +1,10 @@
+public class HW1_B {
+    public static void main(String[] args) {
+        System.out.println("Dam Thai Ninh\t22022522\tQH-2022-I/CQ-AI1\th22022522\t22022522@vnu.edu.vn");
+        for (int i = 9; i >= 1; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
